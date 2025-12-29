d71c6f9ce230678dad69c9dfdccb9dcef96a75bc
Berika Mumcu <berikamumcu23@gmail.com>
2025-12-29 16:25:30 +0300

    React alışveriş sepeti projesi

---
+++ README.md
@@ -0,0 +1,21 @@
+# StoreApp – React Alışveriş Sepeti
+
+React kullanılarak geliştirilmiş basit bir e-ticaret alışveriş sepeti uygulamasıdır.
+
+##  Özellikler
+- Ürün listeleme
+- Sepete ürün ekleme
+- Aynı ürün tekrar eklenirse adet artırma
+- Sepet toplam tutarını otomatik hesaplama
+- Bootstrap ile responsive tasarım
+
+## Kullanılan Teknolojiler
+- React
+- JavaScript (ES6)
+- Bootstrap
+- useState Hook
+
+##  Kurulum
+```bash
+npm install
+npm run dev
---
+++ src/data.js
@@ -0,0 +1,90 @@
+
+export const items = [
+  {
+    id: 1,
+    title: "Kablosuz Kulaklık",
+    price: 4299,
+    image: "/images/1.jpg",
+    description: "Yüksek ses kalitesi, uzun pil ömrü."
+  },
+  {
+    id: 2,
+    title: "Akıllı Saat",
+    price: 5699,
+    image: "/images/2.jpg",
+    description: "Sağlık takibi ve şık tasarım."
+  },
+  {
+    id: 3,
+    title: "Laptop Standı",
+    price: 1499,
+    image: "/images/3.jpg",
+    description: "Ergonomik alüminyum laptop standı."
+  },
+  {
+    id: 4,
+    title: "Klavye",
+    price: 3799,
+    image: "/images/4.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 5,
+    title: "Fotoğraf Makinesi",
+    price: 19000,
+    image: "/images/5.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 6,
+    title: "Hoparlör",
+    price: 1799,
+    image: "/images/6.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 7,
+    title: "Tablet",
+    price: 18499,
+    image: "/images/7.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 8,
+    title: "Batarya",
+    price: 2300,
+    image: "/images/8.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 9,
+    title: "İphone 15",
+    price: 50499,
+    image: "/images/9.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 10,
+    title: "Kahve Makinesi",
+    price: 23450,
+    image: "/images/10.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 11,
+    title: "Tarayıcı",
+    price: 20250,
+    image: "/images/11.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  },
+  {
+    id: 12,
+    title: "Hesap Makinesi",
+    price: 450,
+    image: "/images/12.jpg",
+    description: "RGB ışıklı, blue switch mekanik klavye."
+  }
+
+];
+
+
